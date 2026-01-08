e3a6c605ea609c3482a9c159c828597701ba4a63
Andrei Kramer-Miehe <andrei.kramer@scilifelab.se>
2026-01-08 14:10:00 +0100

    more tests

--- tests/rpnc.sh
+++ tests/rpnc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "TPA version 14"
-echo "1..3"
+echo "1..42"
 
 i=$((1))
 
@@ -16,6 +16,15 @@ i=$((i+1))
 x=$(./rpnc -r '1;2;3' | awk -F '#' '{print $1}' | tr -d '\t')
 [ "$x" = "1;2;3;0" ] && echo "ok $i - the '-r' flag works as expected" || echo "not ok $i - the '-r' flag doesn't work right: «$x» =/= 1;2;3;0"
 
-i=$((i+1))
 
-
+for f in acos acosh asin asinh atan atanh cbrt ceil cos cosh erf erfc exp exp2 expm1 fabs floor lgamma log log10 log1p log2 logb nearbyint rint round sin sinh sqrt tan tanh tgamma trunc j0 j1 y0 y1 significand exp10
+do
+	i=$((i+1))
+	x=$(./rpnc -d "1.0 $f")
+	q=$?
+	if [ $q -eq 0 ]; then
+		echo "ok $i - function $f exists, $f(1.0) returns some value ($x);"
+	else
+		echo "not ok $i - function $f should exist but doesn't ($x)"
+	fi
+done
